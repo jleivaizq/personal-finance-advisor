53ce340971ffaa177049fc0d2e2ecde64b802939
Jose A. Leiva <jleivaizq@gmail.com>
2021-08-07 14:28:07 +0200

    Reset modules to use cli operations instead

--- pfa-server/src/app.module.ts
+++ pfa-server/src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountsController } from './accounts/accounts.controller';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 @Module({
   imports: [
     MikroOrmModule.forRoot(),
   ],
-  controllers: [AppController, AccountsController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
